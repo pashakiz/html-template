9ebe613554c3fa34305ba772bb5a6f69d8e319e3
Pavel Kizernis <pashakiz@gmail.com>
2016-07-09 20:36:41 +0300

    edit headers in mail php

--- mail.php
+++ mail.php
@@ -2,12 +2,19 @@
 
 $recepient = "pashakiz@gmail.com";
 $sitename = "SyncroCity";
+$pagetitle = "Новая заявка с сайта \"$sitename\"";
 
 $name = trim($_POST["name"]);
 $phone = trim($_POST["phone"]);
 $text = trim($_POST["text"]);
+$ip = $_SERVER["REMOTE_ADDR"];
 
-$EOL = "\r\n";
-$pagetitle = "Новая заявка с сайта \"$sitename\"";
-$message = "Имя: $name $EOLТелефон: $phone $EOLТекст: $text";
-mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"$EOL From: $name <$recepient>");
+$message = "Имя: $name \r\nТелефон: $phone \r\nТекст: $text \r\nIP: $ip";
+
+$headers =	'MIME-Version: 1.0. '."\r\n" .
+			'Content-type: text/plain; charset=utf-8'. "\r\n" .
+			'From: '.$name.' <no-reply@zem-krym.ru>' . "\r\n" .
+			'Reply-To: no-reply@zem-krym.ru' . "\r\n" .
+			'X-Mailer: PHP/' . phpversion();
+
+mail($recepient, $pagetitle, $message, $headers);
